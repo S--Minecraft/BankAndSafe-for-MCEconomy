ef948780e6538a22a2dee138ae2d9c2cd4d15cdf
S--Minecraft <dainihon.koukoku@gmail.com>
2014-06-27 21:41:09 +0900

    Updated Texture With Sides

--- kosaki/bankandsafe/blocks/BlockSafe.java
+++ kosaki/bankandsafe/blocks/BlockSafe.java
@@ -13,9 +13,12 @@ public class BlockSafe extends Block
 		super(par1, par2Material);
 		this.setCreativeTab(BankAndSafe.tabBankAndSafe);
 		this.setUnlocalizedName("blockSafe");
-		this.setTextureName("bankandsafe:Safe - " + BankAndSafe.textureSizeFile);
+		//this.setTextureName("bankandsafe:Safe - " + BankAndSafe.textureSizeFile);
 	}
-
+	
+	/**
+	 *GUI
+	 */
 	@Override
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float disX, float disY, float disZ)
 	{
@@ -26,4 +29,34 @@ public class BlockSafe extends Block
 		}
 		return true;
 	}
+	
+	
+	/**
+	 *テクスチャ
+	 */
+	@Override
+    public void onBlockPlacedBy(World par1World, int par2, int par3, int par4, EntityLivingBase par5EntityLivingBase, ItemStack par6ItemStack)
+    {
+        //向きがあるように設定・東西南北固定をなくす
+        int l = MathHelper.floor_double((double)(par5EntityLivingBase.rotationYaw * 4.0F / 360.0F) + 2.5D) & 3;
+        par1World.setBlockMetadataWithNotify(par2, par3, par4, l, 2);
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public Icon getIcon(int par1, int par2)
+    {
+        //表示面の方向を決定
+        return par1 == 1 ? this.blockIcon : (par1 == 0 ? this.blockIcon : (par2 == 2 && par1 == 2 ? this.blockFront : (par2 == 3 && par1 == 5 ? this.blockFront : (par2 == 0 && par1 == 3 ? this.blockFront : (par2 == 1 && par1 == 4 ? this.blockFront : this.blockIcon)))));
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IconRegister par1IconRegister)
+    {
+        //表示面のテクスチャの設定
+        //blockFront:前面　blockIcon:その他
+        this.blockIcon = par1IconRegister.registerIcon("BankAndSafe:Side - " + BankAndSafe.textureSizeFile);
+        this.blockFront = par1IconRegister.registerIcon("BankAndSafe:Safe - " + BankAndSafe.textureSizeFile);
+    }
 }
